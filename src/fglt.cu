11ba418b0763ef355d6264755a5408bec6c0abe6
isidorostsa <tsa.isidoros@gmail.com>
2023-02-24 10:13:50 +0200

    fglt main

--- src/fglt.cu
+++ src/fglt.cu
@@ -15,6 +15,14 @@
 #include "common/printing.hpp"
 #include "common/device_csr_wrapper.hpp"
 
+#define time_bench(func, name) \
+    {\
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
+    func;\
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
+    std::cout << "Time taken for " << #name << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms" << std::endl;\
+    }
+
 #define CHECK_CUDA(call)                                               \
     {                                                                  \
         cudaError_t status = (call);                                   \
@@ -38,6 +46,50 @@
     }
 
 #define computeType CUDA_R_32F
+
+void get_c3_v2_3(const h_csr &A, int *c3)
+{  
+  std::fill(c3, c3 + A.get_rows(), 0);
+
+  for (int i = 0; i < A.get_rows(); i++)
+  {
+    const int i_nb_start = A.offsets[i];
+    const int i_nb_end = A.offsets[i + 1];
+
+    for (int i_nb_idx = i_nb_start; i_nb_idx < i_nb_end; i_nb_idx++)
+    {
+      const int j = A.positions[i_nb_idx];
+      if (i < j) break;
+
+      const int j_nb_start = A.offsets[j];
+      const int j_nb_end = A.offsets[j + 1];
+
+      int _i_nb_idx = i_nb_start;
+      int _j_nb_idx = j_nb_start;
+
+      while (_i_nb_idx < i_nb_end && _j_nb_idx < j_nb_end)
+      {
+        const int _i_nb_pos = A.positions[_i_nb_idx];
+        const int _j_nb_pos = A.positions[_j_nb_idx];
+
+        if(_i_nb_pos > i || _j_nb_pos > j) break;
+
+        if (_i_nb_pos > _j_nb_pos)
+        {
+          _j_nb_idx++;
+        }
+        else if (_i_nb_pos < _j_nb_pos)
+        {
+          _i_nb_idx++;
+        }
+        else
+        {
+          c3[i]++, c3[j]++, c3[_i_nb_pos]++,  _i_nb_idx++, _j_nb_idx++;
+        }
+      }
+    }
+  }
+}
 
 struct d3_trans
 {
@@ -78,8 +130,6 @@ int main(int argc, char *argv[])
     std::cout << "A Sparsity: " << (100 * (1 - ((float)h_A.nnz / ((float)(n) * (float)n)))) << "%" << std::endl;
     std::cout << "A positions: " << h_A.positions.size() << std::endl;
 
-    
-
     // DEVICE DATA
     d_cusparse_csr d_A(h_A);
 
@@ -89,11 +139,11 @@ int main(int argc, char *argv[])
 
                 A_2 = A*A COMPUTATION
 
-    */
     d_cusparse_csr d_A2 = d_cusparse_csr::multiply(d_A, d_A, handle);
 
     std::cout << "A2 = " << std::endl;
     printCSR(d_A2.get_offsets(), d_A2.get_positions(), d_A2.get_values(), n, n, d_A2.get_nnz());
+    */
 
     /*
 
@@ -104,11 +154,6 @@ int main(int argc, char *argv[])
     // make a copy of d_A_offs but with floats
     thrust::device_vector<float> d_A_offs_float(d_A.get_offsets());
 
-    //std::cout << "d_A_offs_float: " << d_A_offs_float;
-
-    // const int num_blocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    // adjDif<<<num_blocks, BLOCK_SIZE>>>(d_A_offs_float.data().get(), d_p1.data().get(), n);
-
     thrust::transform(
         d_A_offs_float.begin() + 1, d_A_offs_float.end(),
         d_A_offs_float.begin(), // d_A_offs_float.end()-1,
@@ -117,34 +162,17 @@ int main(int argc, char *argv[])
 
     std::cout << "p1: " << d_p1;
 
-    // DONE
-    /*
-
-            HADAMARD PRODUCT OF A2 * A
-
-    */
-    // Copy A2 to host
-    h_csr h_A2(d_A2);
-    // Create space for hadamard product
-    h_csr h_C3 = h_csr::hadamard(h_A, h_A2);
-
-    // printCSR
-    std::cout << "C3 = " << std::endl;
-    printCSR(h_C3.offsets, h_C3.positions, h_C3.values, n, n, h_C3.nnz);
-
-    d_cusparse_csr d_C3(h_C3);
-
-    // DONE
-
     /*
 
                 CALCULATE c3
 
     */
-    thrust::device_vector<float> d_e(n, 1.0f);
-    thrust::device_vector<float> d_c3 = d_cusparse_csr::multiply(d_C3, d_e, handle, 0.5f, 0.0f);
 
-    std::cout << "c3: " << d_c3;
+    thrust::host_vector<int> h_c3(n);
+        get_c3_v2_3(h_A, h_c3.data());
+    std::cout << "c3: " << h_c3;
+
+        thrust::device_vector<float> d_c3 = h_c3;
     /*
 
                 CALCULATE p2
@@ -152,16 +180,17 @@ int main(int argc, char *argv[])
     */
     thrust::device_vector<float> d_Ap1 = d_cusparse_csr::multiply(d_A, d_p1, handle);
 
+
     std::cout << "Ap1: " << d_Ap1;
 
     thrust::device_vector<float> d_p2(n);
 
-    thrust::transform(
-        d_Ap1.begin(), d_Ap1.end(),
-        d_p1.begin(),
-        d_p2.begin(),
-        thrust::minus<float>()
-    );
+        thrust::transform(
+            d_Ap1.begin(), d_Ap1.end(),
+            d_p1.begin(),
+            d_p2.begin(),
+            thrust::minus<float>()
+        );
 
     std::cout << "p2: " << d_p2;
     /*
@@ -171,15 +200,12 @@ int main(int argc, char *argv[])
     */
 
     thrust::device_vector<float> d_d2(n);
-
-    thrust::transform(
-        d_p2.begin(),
-        d_p2.end(),
-        d_c3.begin(),
-        d_d2.begin(),
-        d2_trans()
-    );
-
+        thrust::transform(
+            d_p2.begin(),
+            d_p2.end(),
+            d_c3.begin(),
+            d_d2.begin(),
+            d2_trans()        );
     std::cout << "d2: " << d_d2;
 
     /*
@@ -187,14 +213,12 @@ int main(int argc, char *argv[])
     */
 
     thrust::device_vector<float> d_d3(n);
-
     thrust::transform(
         d_p1.begin(),
         d_p1.end(),
         d_c3.begin(),
         d_d3.begin(),
         d3_trans());
-
     std::cout << "d3: " << d_d3;
 
     // DONE
