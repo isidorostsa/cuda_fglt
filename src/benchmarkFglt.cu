121a69f456476d51e973be3177c7f39c56547d57
isidorostsa <tsa.isidoros@gmail.com>
2023-02-24 22:32:04 +0200

    working gpu

--- src/benchmarkFglt.cu
+++ src/benchmarkFglt.cu
@@ -54,30 +54,30 @@ struct d2_trans
     }
 };
 
-void get_c3_v2_3(const h_csr &A, int *c3)
-{  
-  std::fill(c3, c3 + A.get_rows(), 0);
-
-  for (int i = 0; i < A.get_rows(); i++)
+__global__ void c3_kernel(const int *offsets, const int *positions, int *c3, int n)
+{    
+  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x)
   {
-    const int i_nb_start = A.offsets[i];
-    const int i_nb_end = A.offsets[i + 1];
+    const int i_nb_start = offsets[i];
+    const int i_nb_end = offsets[i + 1];
 
+    int amt_i = 0;
     for (int i_nb_idx = i_nb_start; i_nb_idx < i_nb_end; i_nb_idx++)
     {
-      const int j = A.positions[i_nb_idx];
+      const int j = positions[i_nb_idx];
       if (i < j) break;
 
-      const int j_nb_start = A.offsets[j];
-      const int j_nb_end = A.offsets[j + 1];
+      const int j_nb_start = offsets[j];
+      const int j_nb_end = offsets[j + 1];
 
       int _i_nb_idx = i_nb_start;
       int _j_nb_idx = j_nb_start;
 
+      int amt_j = 0;
       while (_i_nb_idx < i_nb_end && _j_nb_idx < j_nb_end)
       {
-        const int _i_nb_pos = A.positions[_i_nb_idx];
-        const int _j_nb_pos = A.positions[_j_nb_idx];
+        const int _i_nb_pos = positions[_i_nb_idx];
+        const int _j_nb_pos = positions[_j_nb_idx];
 
         if(_i_nb_pos > i || _j_nb_pos > j) break;
 
@@ -91,11 +91,35 @@ void get_c3_v2_3(const h_csr &A, int *c3)
         }
         else
         {
-          c3[i]++, c3[j]++, c3[_i_nb_pos]++,  _i_nb_idx++, _j_nb_idx++;
+          amt_j++, atomicAdd(&c3[_i_nb_pos], 1),  _i_nb_idx++, _j_nb_idx++;
         }
       }
+
+      amt_i += amt_j;
+
+      atomicAdd(&c3[j], amt_j);
     }
+
+    atomicAdd(&c3[i], amt_i);
   }
+}
+
+thrust::device_vector<int> get_c3_v3(const d_cusparse_csr& A) {
+    const int n = A.get_rows();
+
+    thrust::device_vector<int> d_c3(n);
+
+    const int *d_A_offs = A.get_offsets().data().get();
+    const int *d_A_pos = A.get_positions().data().get();
+
+    int *d_c3_ptr = d_c3.data().get();
+
+    const int threads_per_block = 256;
+    const int blocks = (n + threads_per_block - 1) / threads_per_block / 10;
+
+    c3_kernel<<<blocks, threads_per_block>>>(d_A_offs, d_A_pos, d_c3_ptr, n);
+
+    return d_c3;
 }
 
 thrust::device_vector<
@@ -126,9 +150,8 @@ thrust::device_vector<
                 CALCULATE c3
     */
 
-    thrust::host_vector<int> h_c3(n);
-    get_c3_v2_3(h_A, h_c3.data());
-    thrust::device_vector<int> d_c3 = h_c3;
+    thrust::device_vector<int> d_c3_int = get_c3_v3(d_A);
+    thrust::device_vector<float> d_c3(d_c3_int);
 
     /*
                 CALCULATE p2
@@ -169,6 +192,73 @@ thrust::device_vector<
     return {};
 }
 
+thrust::device_vector<
+    thrust::device_vector<float>
+> fglt(const d_cusparse_csr& d_A) {
+    cusparseHandle_t handle;
+    CHECK_CUSPARSE(cusparseCreate(&handle))
+
+    const int n = d_A.get_rows();
+    /*
+                CALCULATE p1
+    */
+    thrust::device_vector<float> d_p1(n);
+    // make a copy of d_A_offs but with floats, because cusparse only accepts floats
+    thrust::device_vector<float> d_A_offs_float(d_A.get_offsets());
+
+    // adjecent difference
+    thrust::transform(
+        d_A_offs_float.begin() + 1, d_A_offs_float.end(),
+        d_A_offs_float.begin(), // d_A_offs_float.end()-1,
+        d_p1.begin(),
+        thrust::minus<float>()
+    );
+
+    /*
+                CALCULATE c3
+    */
+
+    thrust::device_vector<int> d_c3_int = get_c3_v3(d_A);
+    thrust::device_vector<float> d_c3(d_c3_int);
+
+    /*
+                CALCULATE p2
+    */
+    thrust::device_vector<float> d_Ap1 = d_cusparse_csr::multiply(d_A, d_p1, handle);
+    thrust::device_vector<float> d_p2(n);
+    thrust::transform(
+        d_Ap1.begin(), d_Ap1.end(),
+        d_p1.begin(),
+        d_p2.begin(),
+        thrust::minus<float>()
+    );
+
+    /*
+                CALCULATE d2
+    */
+    thrust::device_vector<float> d_d2(n);
+    thrust::transform(
+        d_p2.begin(),
+        d_p2.end(),
+        d_c3.begin(),
+        d_d2.begin(),
+        d2_trans()
+    );
+
+    /*
+                CALCULATE d3
+    */
+    thrust::device_vector<float> d_d3(n);
+    thrust::transform(
+        d_p1.begin(),
+        d_p1.end(),
+        d_c3.begin(),
+        d_d3.begin(),
+        d3_trans()
+    );
+
+    return {};
+}
 #define computeType CUDA_R_32F
 
 int main(int argc, char *argv[])
@@ -187,8 +277,9 @@ int main(int argc, char *argv[])
     std::cout << "A = \n";
     printCSR(h_A);
 
+    const d_cusparse_csr d_A(h_A);
     t1 = std::chrono::high_resolution_clock::now();
-    fglt(h_A);
+    fglt(d_A);
     t2 = std::chrono::high_resolution_clock::now();
     std::cout << "Time taken to compute the fglt: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms" << std::endl;
 }
--- src/stefglt.cu
+++ src/stefglt.cu
@@ -12,14 +12,15 @@
 template <typename F>
 int* run_test(const std::string &name, F f, const h_csr &A)
 {
-  std::cout << name << " : ";
+  std::cout << name << " time: ";
   std::cout.flush();
   int *c3 = new int[A.get_rows()];
   std::fill(c3, c3 + A.get_rows(), 0);
   auto start = std::chrono::high_resolution_clock::now();
   f(A, c3);
   auto end = std::chrono::high_resolution_clock::now();
-  std::cout << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() << "s" << std::endl;
+  auto time_span = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+  std::cout << time_span.count() << " seconds." << std::endl;
   return c3;
 }
 
@@ -230,135 +231,55 @@ void get_c3_v2_3(const h_csr &A, int *c3)
   }
 }
 
-void get_c3_v2_4(const h_csr &A, int *c3)
-{  
-  std::fill(c3, c3 + A.get_rows(), 0);
+#define THREADS_PER_BLOCK 256
 
-  for (int i = 0; i < A.get_rows(); i++)
+__global__ void get_c3_v3(const int *offsets, const int *positions, int *c3, int n)
+{    
+  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x)
   {
-    const int i_nb_start = A.offsets[i];
-    const int i_nb_end = A.offsets[i + 1];
+    const int i_nb_start = offsets[i];
+    const int i_nb_end = offsets[i + 1];
 
+    int amt_i = 0;
     for (int i_nb_idx = i_nb_start; i_nb_idx < i_nb_end; i_nb_idx++)
     {
-      const int j = A.positions[i_nb_idx];
+      const int j = positions[i_nb_idx];
       if (i < j) break;
 
-      const int j_nb_start = A.offsets[j];
-      const int j_nb_end = A.offsets[j + 1];
+      const int j_nb_start = offsets[j];
+      const int j_nb_end = offsets[j + 1];
 
       int _i_nb_idx = i_nb_start;
       int _j_nb_idx = j_nb_start;
 
-      const int i_nb_size = i_nb_end - i_nb_start;
-      const int j_nb_size = j_nb_end - j_nb_start;
-
-      if (i_nb_size > j_nb_size)
+      int amt_j = 0;
+      while (_i_nb_idx < i_nb_end && _j_nb_idx < j_nb_end)
       {
-        const int low = _j_nb_idx;
-        const int high = j_nb_end;
+        const int _i_nb_pos = positions[_i_nb_idx];
+        const int _j_nb_pos = positions[_j_nb_idx];
 
-        while (_i_nb_idx < i_nb_end)
+        if(_i_nb_pos > i || _j_nb_pos > j) break;
+
+        if (_i_nb_pos > _j_nb_pos)
         {
-          const int _i_nb_pos = A.positions[_i_nb_idx];
-          if(_i_nb_pos > i || _i_nb_pos > j) break;
-
-          const int mid = (low + high) / 2;
-          if (A.positions[mid] == _i_nb_pos)
-          {
-            c3[i]++, c3[j]++, c3[_i_nb_pos]++,  _i_nb_idx++, _j_nb_idx++;
-          }
-          else if (A.positions[mid] < _i_nb_pos)
-          {
-            _j_nb_idx = mid + 1;
-          }
-          else
-          {
-            _j_nb_idx = mid;
-          }
+          _j_nb_idx++;
         }
-
+        else if (_i_nb_pos < _j_nb_pos)
+        {
+          _i_nb_idx++;
+        }
+        else
+        {
+          amt_j++, atomicAdd(&c3[_i_nb_pos], 1),  _i_nb_idx++, _j_nb_idx++;
+        }
       }
-    }
-  }
-}
 
+      amt_i += amt_j;
 
-
-
-
-__global__ void inner_loop(const int i, const int i_nb_start, const int i_nb_end, const int *positions, const int *offsets, int *c3)
-{
-  const int thread_num = blockIdx.x * blockDim.x + threadIdx.x;
-  const int i_nb_idx = i_nb_start + thread_num;
-
-  if(i == 0) printf("HERE");
-
-  if (i_nb_idx < i_nb_end){
-    const int j = positions[i_nb_idx];
-    const int j_nb_start = offsets[j];
-    const int j_nb_end = offsets[j + 1];
-
-    int _i_nb_idx = i_nb_idx;
-    int _j_nb_idx = j_nb_start;
-    int amt = 0;
-
-    while (_i_nb_idx < i_nb_end && _j_nb_idx < j_nb_end) {
-      if (positions[_i_nb_idx] == positions[_j_nb_idx]) {
-        amt++, _i_nb_idx++, _j_nb_idx++;
-      } else if (positions[_i_nb_idx] < positions[_j_nb_idx]) {
-        _i_nb_idx++;
-      } else {
-        _j_nb_idx++;
-      }
+      atomicAdd(&c3[j], amt_j);
     }
 
-    atomicAdd(&c3[i], amt);
-  }
-
-}
-
-
-#define THREADS_PER_BLOCK 256
-
-__global__ void get_c3_v3(const int *offsets, const int *positions, int *c3, int n)
-{
-  const int i = blockIdx.x * blockDim.x + threadIdx.x;
-
-  if (i < n) {
-    const int i_nb_start = offsets[i];
-    const int i_nb_end = offsets[i + 1];
-
-    if (i == 0 || i == 1)
-    {
-      printf("HERE");
-    }
-
-
-    int num_threads = i_nb_end - i_nb_start;
-    int num_blocks = (num_threads - 1) / THREADS_PER_BLOCK + 1;
-
-    // launch inner loop and wait for it to finish using a stream
-    inner_loop<<<num_blocks, THREADS_PER_BLOCK>>>(i, i_nb_start, i_nb_end, positions, offsets, c3);
-  }
-}
-
-// we do not want to launch more than 1000 kernels at a time
-__global__ void get_c3_v3_2(const int *offsets, const int *positions, int *c3, int n)
-{
-  for(int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x){
-    const int i_nb_start = offsets[i];
-    const int i_nb_end = offsets[i + 1];
-
-    int num_threads = i_nb_end - i_nb_start;
-    int num_blocks = (num_threads - 1) / THREADS_PER_BLOCK + 1;
-
-    // launch inner loop and wait for it to finish using a stream
-
-    // we do not want to launch more than 1000 kernels at a time
-      inner_loop<<<num_blocks, THREADS_PER_BLOCK>>>(i, i_nb_start, i_nb_end, positions, offsets, c3);
-      // cudaDeviceSynchronize();
-      __syncthreads();
+    atomicAdd(&c3[i], amt_i);
   }
 }
 
@@ -382,33 +303,30 @@ int main(int argc, char **argv)
   const int num_blocks = (h_A.get_rows() + 1023) / 1024;
 
   // CPU
-  int* c3_v1 = run_test("V1", get_c3_v1, h_A);
-  int* c3_v2_2 = run_test("V2_2", get_c3_v2_2, h_A);
-  // int *c3_v2_4 = run_test("V2_4", get_c3_v2_4, h_A);
-  int *c3_binary = run_test("Binary", get_c3_v2_binary, h_A);
-
+  // int* c3_v1 = run_test("V1", get_c3_v1, h_A);
+  int* c3_v2_3 = run_test("V2.3", get_c3_v2_3, h_A);
   // GPU
 
-  // std::cout << "Launching kernel with " << num_blocks << " blocks" << std::endl;
-  // t1 = std::chrono::high_resolution_clock::now();
-  // get_c3_v3_2<<<num_blocks, 1024>>>(d_A.get_offsets().data().get(), d_A.get_positions().data().get(), d_c3.data().get(), h_A.get_rows());
-  // cudaDeviceSynchronize();
-  // t2 = std::chrono::high_resolution_clock::now();
-  // time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-  // std::cout << "GPU time: " << time_span.count() << " seconds." << std::endl;
+  std::cout << "Launching kernel with " << num_blocks << " blocks" << std::endl;
+  auto t1 = std::chrono::high_resolution_clock::now();
+  get_c3_v3<<<1000, 1024>>>(d_A.get_offsets().data().get(), d_A.get_positions().data().get(), d_c3.data().get(), h_A.get_rows());
+  cudaDeviceSynchronize();
+  auto t2 = std::chrono::high_resolution_clock::now();
+  auto time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+  std::cout << "GPU time: " << time_span.count() << " seconds." << std::endl;
 
 
   bool error = false;
 
   for(int i = 0; i < h_A.get_rows(); i++)
   {
-    if (c3_v1[i] != c3_v2_2[i] || c3_v1[i] != c3_v2_4[i] || c3_v1[i] != c3_binary[i])
+    // if(c3_v1[i] != c3_v2_3[i] || c3_v1[i] != d_c3[i])
+    if(c3_v2_3[i] != d_c3[i])
     {
-      std::cout << "Error at " << i << std::endl;
-      std::cout << "c3_v1: " << c3_v1[i] << std::endl;
-      std::cout << "c3_v2_2: " << c3_v2_2[i] << std::endl;
-      std::cout << "c3_v2_4: " << c3_v2_4[i] << std::endl;
-      std::cout << "c3_binary: " << c3_binary[i] << std::endl;
+      std::cout << "Error at " << i << "\n";
+      // std::cout << "CPU V1: " << c3_v1[i] << "\n";
+      std::cout << "CPU V2.3: " << c3_v2_3[i] << "\n";
+      std::cout << "GPU: " << d_c3[i] << "\n";
       error = true;
       break;
     }
