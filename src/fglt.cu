87735da5f0cd3a78372e45109f95df6c2f0a296b
isidorostsa <tsa.isidoros@gmail.com>
2023-02-25 11:43:54 +0200

    replace with old "symbolic" fglt file

--- src/fglt.cu
+++ src/fglt.cu
@@ -1,76 +1,66 @@
-#include <cuda.h>
-#include <cusparse.h>
 #include <iostream>
-#include <iomanip>
+#include <chrono>
+
+#include <cuda.h>
 
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-
 #include <thrust/transform.h>
 
-#include <cstddef>
-
 #include "common/fileio.hpp"
-#include "common/sparse_funcs.hpp"
 #include "common/printing.hpp"
-#include "common/device_csr_wrapper.hpp"
+#include "common/device_csr.hpp"
 
-#define time_bench(func, name) \
-    {\
-    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();\
-    func;\
-    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();\
-    std::cout << "Time taken for " << #name << ": " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "ms" << std::endl;\
+#define time_op(name, op) \
+        start = std::chrono::high_resolution_clock::now(); \
+        op; \
+        end = std::chrono::high_resolution_clock::now(); \
+        std::cout << name << " took " << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << " ms" << std::endl;
+
+struct d3_trans
+{
+    __thrust_exec_check_disable__
+        __host__ __device__ constexpr COMPUTE_TYPE
+        operator()(const COMPUTE_TYPE &lhs, const COMPUTE_TYPE &rhs) const
+    {
+        return (lhs) * (lhs - 1) / 2 -  rhs;
     }
+};
 
-#define CHECK_CUDA(call)                                               \
-    {                                                                  \
-        cudaError_t status = (call);                                   \
-        if (status != cudaSuccess)                                     \
-        {                                                              \
-            printf("CUDA API failed at line %d with error: %s (%d)\n", \
-                   __LINE__, cudaGetErrorString(status), status);      \
-            exit(1);                                                   \
-        }                                                              \
+struct d2_trans
+{
+    __thrust_exec_check_disable__
+        __host__ __device__ constexpr COMPUTE_TYPE
+        operator()(const COMPUTE_TYPE &lhs, const COMPUTE_TYPE &rhs) const
+    {
+        return (lhs) - 2 * rhs;
     }
+};
 
-#define CHECK_CUSPARSE(call)                                                                           \
-    {                                                                                                  \
-        cusparseStatus_t status = call;                                                                \
-        if (status != CUSPARSE_STATUS_SUCCESS)                                                         \
-        {                                                                                              \
-            fprintf(stderr, "cuSparse error %s in file '%s' in line %i : %s.\n",                       \
-                    cusparseGetErrorName(status), __FILE__, __LINE__, cusparseGetErrorString(status)); \
-            exit(1);                                                                                   \
-        }                                                                                              \
-    }
-
-#define computeType CUDA_R_32F
-
-void get_c3_v2_3(const h_csr &A, int *c3)
-{  
-  std::fill(c3, c3 + A.get_rows(), 0);
-
-  for (int i = 0; i < A.get_rows(); i++)
+__global__ void c3_kernel(const int *offsets, const int *positions, COMPUTE_TYPE *c3, int n)
+{    
+  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += blockDim.x * gridDim.x)
   {
-    const int i_nb_start = A.offsets[i];
-    const int i_nb_end = A.offsets[i + 1];
+    const int i_nb_start = offsets[i];
+    const int i_nb_end = offsets[i + 1];
 
+    int amt_i = 0;
     for (int i_nb_idx = i_nb_start; i_nb_idx < i_nb_end; i_nb_idx++)
     {
-      const int j = A.positions[i_nb_idx];
+      const int j = positions[i_nb_idx];
       if (i < j) break;
 
-      const int j_nb_start = A.offsets[j];
-      const int j_nb_end = A.offsets[j + 1];
+      const int j_nb_start = offsets[j];
+      const int j_nb_end = offsets[j + 1];
 
       int _i_nb_idx = i_nb_start;
       int _j_nb_idx = j_nb_start;
 
+      int amt_j = 0;
       while (_i_nb_idx < i_nb_end && _j_nb_idx < j_nb_end)
       {
-        const int _i_nb_pos = A.positions[_i_nb_idx];
-        const int _j_nb_pos = A.positions[_j_nb_idx];
+        const int _i_nb_pos = positions[_i_nb_idx];
+        const int _j_nb_pos = positions[_j_nb_idx];
 
         if(_i_nb_pos > i || _j_nb_pos > j) break;
 
@@ -84,148 +74,146 @@ void get_c3_v2_3(const h_csr &A, int *c3)
         }
         else
         {
-          c3[i]++, c3[j]++, c3[_i_nb_pos]++,  _i_nb_idx++, _j_nb_idx++;
+          amt_j++, atomicAdd(&c3[_i_nb_pos], 1),  _i_nb_idx++, _j_nb_idx++;
         }
       }
+
+      amt_i += amt_j;
+
+      atomicAdd(&c3[j], amt_j);
     }
+
+    atomicAdd(&c3[i], amt_i);
   }
 }
 
-struct d3_trans
-{
-    __thrust_exec_check_disable__
-        __host__ __device__ constexpr float
-        operator()(const float &lhs, const float &rhs) const
-    {
-        return (lhs) * (lhs - 1) / 2 -  rhs;
-    }
-};
+thrust::device_vector<COMPUTE_TYPE> get_c3_v3(const d_csr& A) {
+    const int n = A.get_rows();
 
-struct d2_trans
-{
-    __thrust_exec_check_disable__
-        __host__ __device__ constexpr float
-        operator()(const float &lhs, const float &rhs) const
-    {
-        return (lhs) - 2 * rhs;
-    }
-};
+    thrust::device_vector<COMPUTE_TYPE> d_c3(n);
 
+    const int *d_A_offs = A.get_offsets().data().get();
+    const int *d_A_pos = A.get_positions().data().get();
 
+    COMPUTE_TYPE *d_c3_ptr = d_c3.data().get();
 
-int main(int argc, char *argv[])
-{
+    const int SmSize = 16;
+    const int threadsPerBlock = 256;
+    const int threadsPerSM = threadsPerBlock * SmSize;
 
-    std::string filename(argv[1]); 
-    // HOST DATA
-    h_csr h_A = loadSymmFileToCsr(filename);
+    const int FullSMs = (n + threadsPerSM - 1) / threadsPerSM;
 
-    thrust::host_vector<float> h_A_vals(h_A.nnz, 1.0f);
+    c3_kernel<<<SmSize*FullSMs, threadsPerBlock>>>(d_A_offs, d_A_pos, d_c3_ptr, n);
+    // c3_kernel<<<1, 1>>>(d_A_offs, d_A_pos, d_c3_ptr, n);
+    cudaDeviceSynchronize();
 
-    const int n = h_A.rows;
-    const int nnz = h_A.nnz;
+    return d_c3;
+}
 
-    std::cout << "A = " << std::endl;
-    printCSR(h_A.offsets, h_A.positions, h_A_vals, n, n, nnz);
-    std::cout << "A Sparsity: " << (100 * (1 - ((float)h_A.nnz / ((float)(n) * (float)n)))) << "%" << std::endl;
-    std::cout << "A positions: " << h_A.positions.size() << std::endl;
+thrust::host_vector<
+    thrust::device_vector<COMPUTE_TYPE>
+> fglt(const d_csr& d_A) {
 
-    // DEVICE DATA
-    d_cusparse_csr d_A(h_A);
+    std::chrono::system_clock::time_point start;
+    std::chrono::system_clock::time_point end;
 
-    cusparseHandle_t handle;
-    CHECK_CUSPARSE(cusparseCreate(&handle))
+    const int n = d_A.get_rows();
     /*
-
-                A_2 = A*A COMPUTATION
-
-    d_cusparse_csr d_A2 = d_cusparse_csr::multiply(d_A, d_A, handle);
-
-    std::cout << "A2 = " << std::endl;
-    printCSR(d_A2.get_offsets(), d_A2.get_positions(), d_A2.get_values(), n, n, d_A2.get_nnz());
-    */
-
-    /*
-
                 CALCULATE p1
-
     */
-    thrust::device_vector<float> d_p1(n);
-    // make a copy of d_A_offs but with floats
-    thrust::device_vector<float> d_A_offs_float(d_A.get_offsets());
-
+time_op("p1",
+    thrust::device_vector<COMPUTE_TYPE> d_p1(n);
+    // adjecent difference
     thrust::transform(
-        d_A_offs_float.begin() + 1, d_A_offs_float.end(),
-        d_A_offs_float.begin(), // d_A_offs_float.end()-1,
+        d_A.offsets.begin() + 1, d_A.offsets.end(),
+        d_A.offsets.begin(), // d_A_offs_COMPUTE_TYPE.end()-1,
         d_p1.begin(),
-        thrust::minus<float>());
-
-    std::cout << "p1: " << d_p1;
-
+        thrust::minus<COMPUTE_TYPE>()
+    )
+)
     /*
-
                 CALCULATE c3
-
     */
 
-    thrust::host_vector<int> h_c3(n);
-        get_c3_v2_3(h_A, h_c3.data());
-    std::cout << "c3: " << h_c3;
+time_op("c3",
+    thrust::device_vector<COMPUTE_TYPE> d_c3 = get_c3_v3(d_A);
+)
 
-        thrust::device_vector<float> d_c3 = h_c3;
     /*
-
                 CALCULATE p2
-
     */
-    thrust::device_vector<float> d_Ap1 = d_cusparse_csr::multiply(d_A, d_p1, handle);
-
-
-    std::cout << "Ap1: " << d_Ap1;
-
-    thrust::device_vector<float> d_p2(n);
-
-        thrust::transform(
-            d_Ap1.begin(), d_Ap1.end(),
-            d_p1.begin(),
-            d_p2.begin(),
-            thrust::minus<float>()
-        );
-
-    std::cout << "p2: " << d_p2;
+time_op("p2",
+    thrust::device_vector<COMPUTE_TYPE> d_Ap1 = d_csr::spmv_symbolic(d_A, d_p1);
+    thrust::device_vector<COMPUTE_TYPE> d_p2(n);
+    thrust::transform(
+        d_Ap1.begin(), d_Ap1.end(),
+        d_p1.begin(),
+        d_p2.begin(),
+        thrust::minus<COMPUTE_TYPE>()
+    );
+)
     /*
-
                 CALCULATE d2
-
     */
-
-    thrust::device_vector<float> d_d2(n);
-        thrust::transform(
-            d_p2.begin(),
-            d_p2.end(),
-            d_c3.begin(),
-            d_d2.begin(),
-            d2_trans()        );
-    std::cout << "d2: " << d_d2;
-
+time_op("d2",
+    thrust::device_vector<COMPUTE_TYPE> d_d2(n);
+    thrust::transform(
+        d_p2.begin(),
+        d_p2.end(),
+        d_c3.begin(),
+        d_d2.begin(),
+        d2_trans()
+    );
+)
     /*
                 CALCULATE d3
     */
-
-    thrust::device_vector<float> d_d3(n);
+time_op("d3",
+    thrust::device_vector<COMPUTE_TYPE> d_d3(n);
     thrust::transform(
         d_p1.begin(),
         d_p1.end(),
         d_c3.begin(),
         d_d3.begin(),
-        d3_trans());
-    std::cout << "d3: " << d_d3;
+        d3_trans()
+    );
+)
 
-    // DONE
+    cudaDeviceSynchronize();
 
-    std::cout << "d1 = " << d_p1;
-    std::cout << "d2 = " << d_d2;
-    std::cout << "d3 = " << d_d3;
-    std::cout << "d4 = " << d_c3;
-    return 0;
+time_op("return vectors move",
+    thrust::host_vector<thrust::device_vector<COMPUTE_TYPE>> return_vector(4);
+    return_vector.push_back(std::move(d_p1));
+    return_vector.push_back(std::move(d_p2));
+    return_vector.push_back(std::move(d_d3));
+    return_vector.push_back(std::move(d_c3));
+)
+    return return_vector;
+}
+
+int main(int argc, char *argv[])
+{
+    std::chrono::system_clock::time_point start;
+    std::chrono::system_clock::time_point end;
+
+    if(argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return 1;
+    }
+    std::string filename(argv[1]); 
+
+    time_op("Loading the file",
+        const h_csr h_A = loadSymmFileToCsr(filename);
+    )
+
+    std::cout << "A = \n";
+    printCSR(h_A);
+
+    time_op("Moving the matrix to the device",
+        const d_csr d_A(h_A);
+    )
+
+    time_op("The whole fglt",
+        thrust::host_vector<thrust::device_vector<COMPUTE_TYPE>> h_fglt = fglt(d_A);
+    )
 }
