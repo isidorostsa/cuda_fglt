8697e51ef0fe33dc725c185e1f06368dc3f61f28
isidorostsa <tsa.isidoros@gmail.com>
2023-03-01 01:07:39 +0200

    taskflow stuff

--- script.sh
+++ script.sh
@@ -15,20 +15,20 @@ echo "TASKFLOW_VERSION"
 echo "___S12___"
 bin/fglt ../assets/s12.mtx
 bin/fglt ../assets/s12.mtx
-bin/fglt ../assets/s12.mtx
+TF_ENABLE_PROFILER=s12_tf.json bin/fglt ../assets/s12.mtx
 echo "___AUTO___"
 bin/fglt ../assets/auto.mtx
 bin/fglt ../assets/auto.mtx
-bin/fglt ../assets/auto.mtx
+TF_ENABLE_PROFILER=auto_tf.json bin/fglt ../assets/auto.mtx
 echo "___GB___"
 bin/fglt ../assets/great-britain_osm.mtx
 bin/fglt ../assets/great-britain_osm.mtx
-bin/fglt ../assets/great-britain_osm.mtx
+TF_ENABLE_PROFILER=gb_tf.json bin/fglt ../assets/great-britain_osm.mtx
 echo "___DL___"
 bin/fglt ../assets/delaunay_n22.mtx
 bin/fglt ../assets/delaunay_n22.mtx
-bin/fglt ../assets/delaunay_n22.mtx
+TF_ENABLE_PROFILER=dl_tf.json bin/fglt ../assets/delaunay_n22.mtx
 echo "___CYT___"
 bin/fglt ../assets/com-Youtube.mtx
 bin/fglt ../assets/com-Youtube.mtx
-bin/fglt ../assets/com-Youtube.mtx
+TF_ENABLE_PROFILER=cY_tf.json bin/fglt ../assets/com-Youtube.mtx
