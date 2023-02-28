9525edda95f2c97c8c3d5959eb1b82acdea7bb1e
johnstef99 <john@johnstef.com>
2023-02-28 22:17:50 +0200

    Update sbatch script

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=15:00
+#SBATCH --time=10:00
 
 module purge
 module load gcc
@@ -11,46 +11,48 @@ module load cuda
 make clean
 make
 
+ASSETS="./assets"
+
 echo "THRUST_VERSION"
 echo "___S12___"
-bin/fglt ../assets/s12.mtx
-bin/fglt ../assets/s12.mtx
-bin/fglt ../assets/s12.mtx
+bin/fglt $ASSETS/s12.mtx
+bin/fglt $ASSETS/s12.mtx
+bin/fglt $ASSETS/s12.mtx
 echo "___AUTO___"
-bin/fglt ../assets/auto.mtx
-bin/fglt ../assets/auto.mtx
-bin/fglt ../assets/auto.mtx
+bin/fglt $ASSETS/auto.mtx
+bin/fglt $ASSETS/auto.mtx
+bin/fglt $ASSETS/auto.mtx
 echo "___GB___"
-bin/fglt ../assets/great-britain_osm.mtx
-bin/fglt ../assets/great-britain_osm.mtx
-bin/fglt ../assets/great-britain_osm.mtx
+bin/fglt $ASSETS/great-britain_osm.mtx
+bin/fglt $ASSETS/great-britain_osm.mtx
+bin/fglt $ASSETS/great-britain_osm.mtx
 echo "___DL___"
-bin/fglt ../assets/delaunay_n22.mtx
-bin/fglt ../assets/delaunay_n22.mtx
-bin/fglt ../assets/delaunay_n22.mtx
+bin/fglt $ASSETS/delaunay_n22.mtx
+bin/fglt $ASSETS/delaunay_n22.mtx
+bin/fglt $ASSETS/delaunay_n22.mtx
 echo "___CYT___"
-bin/fglt ../assets/com-Youtube.mtx
-bin/fglt ../assets/com-Youtube.mtx
-bin/fglt ../assets/com-Youtube.mtx
+bin/fglt $ASSETS/com-Youtube.mtx
+bin/fglt $ASSETS/com-Youtube.mtx
+bin/fglt $ASSETS/com-Youtube.mtx
 
 echo "CUDA_VERSION"
 echo "___S12___"
-bin/fglt_c ../assets/s12.mtx
-bin/fglt_c ../assets/s12.mtx
-bin/fglt_c ../assets/s12.mtx
+bin/fglt_c $ASSETS/s12.mtx
+bin/fglt_c $ASSETS/s12.mtx
+bin/fglt_c $ASSETS/s12.mtx
 echo "___AUTO___"
-bin/fglt_c ../assets/auto.mtx
-bin/fglt_c ../assets/auto.mtx
-bin/fglt_c ../assets/auto.mtx
+bin/fglt_c $ASSETS/auto.mtx
+bin/fglt_c $ASSETS/auto.mtx
+bin/fglt_c $ASSETS/auto.mtx
 echo "___GB___"
-bin/fglt_c ../assets/great-britain_osm.mtx
-bin/fglt_c ../assets/great-britain_osm.mtx
-bin/fglt_c ../assets/great-britain_osm.mtx
+bin/fglt_c $ASSETS/great-britain_osm.mtx
+bin/fglt_c $ASSETS/great-britain_osm.mtx
+bin/fglt_c $ASSETS/great-britain_osm.mtx
 echo "___DL___"
-bin/fglt_c ../assets/delaunay_n22.mtx
-bin/fglt_c ../assets/delaunay_n22.mtx
-bin/fglt_c ../assets/delaunay_n22.mtx
+bin/fglt_c $ASSETS/delaunay_n22.mtx
+bin/fglt_c $ASSETS/delaunay_n22.mtx
+bin/fglt_c $ASSETS/delaunay_n22.mtx
 echo "___CYT___"
-bin/fglt_c ../assets/com-Youtube.mtx
-bin/fglt_c ../assets/com-Youtube.mtx
-bin/fglt_c ../assets/com-Youtube.mtx
+bin/fglt_c $ASSETS/com-Youtube.mtx
+bin/fglt_c $ASSETS/com-Youtube.mtx
+bin/fglt_c $ASSETS/com-Youtube.mtx
