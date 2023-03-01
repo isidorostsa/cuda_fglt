31863d540624aecfaeb46aa0ce200cb87ac0663f
isidorostsa <tsa.isidoros@gmail.com>
2023-03-01 11:39:28 +0200

    script fix

--- script.sh
+++ script.sh
@@ -14,22 +14,22 @@ make
 
 echo "TASKFLOW_VERSION"
 echo "___S12___"
-julia ./src/fglt ../assets/s12.mtx
-julia ./src/fglt ../assets/s12.mtx
-julia ./src/fglt ../assets/s12.mtx
+julia ./src/fglt.jl ../assets/s12.mtx
+julia ./src/fglt.jl ../assets/s12.mtx
+julia ./src/fglt.jl ../assets/s12.mtx
 echo "___AUTO___"
-julia ./src/fglt ../assets/auto.mtx
-julia ./src/fglt ../assets/auto.mtx
-julia ./src/fglt ../assets/auto.mtx
+julia ./src/fglt.jl ../assets/auto.mtx
+julia ./src/fglt.jl ../assets/auto.mtx
+julia ./src/fglt.jl ../assets/auto.mtx
 echo "___GB___"
-julia ./src/fglt ../assets/great-britain_osm.mtx
-julia ./src/fglt ../assets/great-britain_osm.mtx
-julia ./src/fglt ../assets/great-britain_osm.mtx
+julia ./src/fglt.jl ../assets/great-britain_osm.mtx
+julia ./src/fglt.jl ../assets/great-britain_osm.mtx
+julia ./src/fglt.jl ../assets/great-britain_osm.mtx
 echo "___DL___"
-julia ./src/fglt ../assets/delaunay_n22.mtx
-julia ./src/fglt ../assets/delaunay_n22.mtx
-julia ./src/fglt ../assets/delaunay_n22.mtx
+julia ./src/fglt.jl ../assets/delaunay_n22.mtx
+julia ./src/fglt.jl ../assets/delaunay_n22.mtx
+julia ./src/fglt.jl ../assets/delaunay_n22.mtx
 echo "___CYT___"
-julia ./src/fglt ../assets/com-Youtube.mtx
-julia ./src/fglt ../assets/com-Youtube.mtx
-julia ./src/fglt ../assets/com-Youtube.mtx
+julia ./src/fglt.jl ../assets/com-Youtube.mtx
+julia ./src/fglt.jl ../assets/com-Youtube.mtx
+julia ./src/fglt.jl ../assets/com-Youtube.mtx
