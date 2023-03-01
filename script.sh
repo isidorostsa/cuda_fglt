14007256c0165a7c6101b26f497fdae2453a4650
isidorostsa <tsa.isidoros@gmail.com>
2023-03-01 11:32:33 +0200

    julia script

--- script.sh
+++ script.sh
@@ -6,29 +6,30 @@
 #SBATCH --time=15:00
 
 module purge
-module load gcc/9.2.0
-module load cuda/11.1.0
+module load gcc/10.2.0
+module load cuda
+module load julia/1.6.3
 make clean
 make
 
 echo "TASKFLOW_VERSION"
 echo "___S12___"
-bin/fglt ../assets/s12.mtx
-bin/fglt ../assets/s12.mtx
-TF_ENABLE_PROFILER=s12_tf.json bin/fglt ../assets/s12.mtx
+julia ./src/fglt ../assets/s12.mtx
+julia ./src/fglt ../assets/s12.mtx
+julia ./src/fglt ../assets/s12.mtx
 echo "___AUTO___"
-bin/fglt ../assets/auto.mtx
-bin/fglt ../assets/auto.mtx
-TF_ENABLE_PROFILER=auto_tf.json bin/fglt ../assets/auto.mtx
+julia ./src/fglt ../assets/auto.mtx
+julia ./src/fglt ../assets/auto.mtx
+julia ./src/fglt ../assets/auto.mtx
 echo "___GB___"
-bin/fglt ../assets/great-britain_osm.mtx
-bin/fglt ../assets/great-britain_osm.mtx
-TF_ENABLE_PROFILER=gb_tf.json bin/fglt ../assets/great-britain_osm.mtx
+julia ./src/fglt ../assets/great-britain_osm.mtx
+julia ./src/fglt ../assets/great-britain_osm.mtx
+julia ./src/fglt ../assets/great-britain_osm.mtx
 echo "___DL___"
-bin/fglt ../assets/delaunay_n22.mtx
-bin/fglt ../assets/delaunay_n22.mtx
-TF_ENABLE_PROFILER=dl_tf.json bin/fglt ../assets/delaunay_n22.mtx
+julia ./src/fglt ../assets/delaunay_n22.mtx
+julia ./src/fglt ../assets/delaunay_n22.mtx
+julia ./src/fglt ../assets/delaunay_n22.mtx
 echo "___CYT___"
-bin/fglt ../assets/com-Youtube.mtx
-bin/fglt ../assets/com-Youtube.mtx
-TF_ENABLE_PROFILER=cY_tf.json bin/fglt ../assets/com-Youtube.mtx
+julia ./src/fglt ../assets/com-Youtube.mtx
+julia ./src/fglt ../assets/com-Youtube.mtx
+julia ./src/fglt ../assets/com-Youtube.mtx
